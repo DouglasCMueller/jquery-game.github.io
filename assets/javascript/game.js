711f25a2e5f028979eedd987375d4940d353587a
Douglas Mueller <douglasmueller6@gmail.com>
2019-11-06 17:04:59 -0500

    characters added to screen

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,37 +13,46 @@ var badGuysCharactersArray =[
     {name: "James Butler", image: "assets/images/jamesButler.jpg", healthpoints: 50 },
     {name: "Jesse James", image:"assets/images/jesseJames.jpg", healthpoints: 40 },
     {name: "Three Fingered Jack", image:"assets/images/threeFingeredJack.jpg", healthpoints: 30 },
-    
 ];
-
-
-
-
+//define startGame
+function startGame(){
 //generate good guy characters
-
 for (var i = 0; i < goodGuysCharactersArray.length; i++){
     var goodGuyImage = $("<img>");
     goodGuyImage.addClass("goodGuyImageClass");
     goodGuyImage.attr("src",goodGuysCharactersArray[i].image);
-    goodGuyImage.attr("datagoodGuyImageValue",goodGuysCharactersArray[i].healthpoints);
+    goodGuyImage.attr("dataGoodGuyImageValue",goodGuysCharactersArray[i].healthpoints);
     $("#goodGuyCharacters").append(goodGuyImage);
-    
-    console.log(goodGuyImage);
     
     
 }
 //generate  bad guy characters
-
-for (var j = 0; j < goodGuysCharactersArray.length; j++){
+for (var j = 0; j < badGuysCharactersArray.length; j++){
     var badGuyImage = $("<img>");
     badGuyImage.addClass("badGuyImageClass");
-    badGuyImage.attr("src",badGuysCharactersArray[j].image);
-    badGuyImage.attr("databadGuyImageValue",badGuysCharactersArray[j].healthpoints);
+    badGuyImage.attr("src", badGuysCharactersArray[j].image);
+    badGuyImage.attr("dataBadGuyImageValue",badGuysCharactersArray[j].healthpoints);
     $("#badGuyCharacters").append(badGuyImage);
-    console.log(badGuyImage);
-   
 }
+//enable click on good guy characters
+$(".goodGuyImageClass").on("click", function(){
+    //store value of each gem
+    var goodGuyFightingValue = ($(this).attr("dataGoodGuyImageValue"));
+    //assign numeric value to gem
+    goodGuyFightingValue = parseInt(goodGuyFightingValue);
+    console.log(goodGuyFightingValue)
+    $("#goodGuyFighting").append(this);
 
+});
+$(".badGuyImageClass").on("click", function(){
+    //store value of each gem
+    var badGuyFightingValue = ($(this).attr("dataBadGuyImageValue"));
+    //assign numeric value to gem
+    badGuyFightingValue = parseInt(badGuyFightingValue);
+    console.log(badGuyFightingValue)
+    $("#badGuyFighting").append(this);
 
-
+});
+}
+startGame();
 });
