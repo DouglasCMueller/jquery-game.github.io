dd30a589cca6069185ba2ec8fea11a1d4fdf5944
Douglas Mueller <douglasmueller6@gmail.com>
2019-11-07 17:31:45 -0500

    gunfighter chosen and dueling area created

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,15 +4,16 @@ $(document).ready(function() {
 
 
 var goodGuysCharactersArray =[
-    {name: "Annie Oakley", image: "assets/images/annieOakley.jpg", healthpoints: 100},
-    {name: "Ben Thompson", image:"assets/images/benThompson.jpg", healthpoints: 110},
-    {name: "Wyatt Earp", image:"assets/images/wyattEarp.jpg", healthpoints: 120},
-    {name: "Wild Bill Hickock", image:"assets/images/wildBillHickock.jpg", healthpoints: 130},
+    {name: "Annie Oakley", image: "assets/images/annieOakley.jpg", healthpoints: 105},
+    {name: "Ben Thompson", image:"assets/images/benThompson.jpg", healthpoints: 115},
+    {name: "Wyatt Earp", image:"assets/images/wyattEarp.jpg", healthpoints: 125},
+    {name: "Wild Bill Hickock", image:"assets/images/wildBillHickock.jpg", healthpoints: 135},
 ];
 var badGuysCharactersArray =[
-    {name: "James Butler", image: "assets/images/jamesButler.jpg", healthpoints: 50 },
-    {name: "Jesse James", image:"assets/images/jesseJames.jpg", healthpoints: 40 },
-    {name: "Three Fingered Jack", image:"assets/images/threeFingeredJack.jpg", healthpoints: 30 },
+    {name: "James Butler", image: "assets/images/jamesButler.jpg", healthpoints: 100 },
+    {name: "Jesse James", image:"assets/images/jesseJames.jpg", healthpoints: 110 },
+    {name: "Three Fingered Jack", image:"assets/images/threeFingeredJack.jpg", healthpoints: 120 },
+    {name: "John Wesley Hardin", image:"assets/images/johnWesleyHardin.jpg", healthpoints: 130}
 ];
 //define startGame
 function startGame(){
@@ -22,9 +23,8 @@ for (var i = 0; i < goodGuysCharactersArray.length; i++){
     goodGuyImage.addClass("goodGuyImageClass");
     goodGuyImage.attr("src",goodGuysCharactersArray[i].image);
     goodGuyImage.attr("dataGoodGuyImageValue",goodGuysCharactersArray[i].healthpoints);
+    goodGuyImage.attr("dataGoodGuyImageName",goodGuysCharactersArray[i].name);
     $("#goodGuyCharacters").append(goodGuyImage);
-    
-    
 }
 //generate  bad guy characters
 for (var j = 0; j < badGuysCharactersArray.length; j++){
@@ -32,25 +32,35 @@ for (var j = 0; j < badGuysCharactersArray.length; j++){
     badGuyImage.addClass("badGuyImageClass");
     badGuyImage.attr("src", badGuysCharactersArray[j].image);
     badGuyImage.attr("dataBadGuyImageValue",badGuysCharactersArray[j].healthpoints);
+    badGuyImage.attr("dataBadGuyImageName",badGuysCharactersArray[j].name);
     $("#badGuyCharacters").append(badGuyImage);
 }
 //enable click on good guy characters
 $(".goodGuyImageClass").on("click", function(){
-    //store value of each gem
+    //store value of each good guy
     var goodGuyFightingValue = ($(this).attr("dataGoodGuyImageValue"));
-    //assign numeric value to gem
+    //assign numeric value to good guy
     goodGuyFightingValue = parseInt(goodGuyFightingValue);
     console.log(goodGuyFightingValue)
-    $("#goodGuyFighting").append(this);
+    //assign name to good guy
+     var goodGuyName = ($(this)).attr("dataGoodGuyImageName");
+    console.log(goodGuyName);
+    $("#goodGuyFighting").html(this);
+    $("#goodGuyNameDisplayed").html(goodGuyName);
 
 });
 $(".badGuyImageClass").on("click", function(){
-    //store value of each gem
+    //store value of each bad guy
     var badGuyFightingValue = ($(this).attr("dataBadGuyImageValue"));
-    //assign numeric value to gem
+    //assign numeric value to bad guy
     badGuyFightingValue = parseInt(badGuyFightingValue);
     console.log(badGuyFightingValue)
+     //assign name to good guy
+     var badGuyName = ($(this)).attr("dataBadGuyImageName");
+    console.log(badGuyName);
     $("#badGuyFighting").append(this);
+    $("#badGuyNameDisplayed").html(badGuyName);
+
 
 });
 }
