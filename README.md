e512a46edfd66e86cf53e7db6e21eaecddf57143
Douglas Mueller <douglasmueller6@gmail.com>
2019-11-09 09:40:25 -0500

    added reset button and added values to characters

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 Wild West RPG!
 Developer: Douglas C. Mueller
 This is an RPG style game with characters from the Wild West of the United States. It is written in javascript with a jquery library.  To play, the user clicks a "Good Guy" character.  User then gets a choice of "Bad Guy" characted to engage in a gun fight.  The player must defeat three "Bad Guys" in order to win the game.
+issues:
+1. I added "health points/attack power/counter attack power" to each gunfighter, but I didn't not put those values into the game play.
+2. I added a reset button, but can't get it to work. 
+3. When I created the character's images, I couldn't figure out how to add "health points" or "name" directly to the image <div>.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,22 +5,24 @@ var goodGuyChosen = false;
 var badGuyChosen = false;
 var goodGuyName;
 var badGuyName;
+var goodGuyHealthPointsValue;
+var badGuyHealthPointsValue;
 var randomNumber = 0;
 var goodGuyNameDueling ="";
 var delayButtonAlert;
 var goodGuysCharactersArray =[
-    {name: "Annie Oakley", image: "assets/images/annieOakley.jpg", healthpoints: 105},
-    {name: "Ben Thompson", image:"assets/images/benThompson.jpg", healthpoints: 115},
-    {name: "Wyatt Earp", image:"assets/images/wyattEarp.jpg", healthpoints: 125},
-    {name: "Wild Bill Hickock", image:"assets/images/wildBillHickock.jpg", healthpoints: 135},
+    {name: "Annie Oakley", image: "assets/images/annieOakley.jpg", healthpoints: 105, attackPower: 15, counterAttackPower: 5},
+    {name: "Ben Thompson", image:"assets/images/benThompson.jpg", healthpoints: 115, attackPower: 25, counterAttackPower: 15},
+    {name: "Wyatt Earp", image:"assets/images/wyattEarp.jpg", healthpoints: 125, attackPower: 35, counterAttackPower: 25},
+    {name: "Wild Bill Hickock", image:"assets/images/wildBillHickock.jpg", healthpoints: 135, attackPower: 45, counterAttackPower: 35},
 ];
 var badGuysCharactersArray =[
-    {name: "James Butler", image: "assets/images/jamesButler.jpg", healthpoints: 100 },
-    {name: "Jesse James", image:"assets/images/jesseJames.jpg", healthpoints: 110 },
-    {name: "Three Fingered Jack", image:"assets/images/threeFingeredJack.jpg", healthpoints: 120 },
-    {name: "John Wesley Hardin", image:"assets/images/johnWesleyHardin.jpg", healthpoints: 130}
+    {name: "James Butler", image: "assets/images/jamesButler.jpg", healthpoints: 100, attackPower: 20, counterAttackPower: 15},
+    {name: "Jesse James", image:"assets/images/jesseJames.jpg", healthpoints: 110, attackPower: 30, counterAttackPower: 25},
+    {name: "Three Fingered Jack", image:"assets/images/threeFingeredJack.jpg", healthpoints: 120, attackPower: 40, counterAttackPower: 35},
+    {name: "John Wesley Hardin", image:"assets/images/johnWesleyHardin.jpg", healthpoints: 130, attackPower: 50, counterAttackPower: 45}
 ];
-
+var resetButton = $('.reset');
 //define startGame
 function startGame(){
 //generate good guy characters
@@ -43,12 +45,13 @@ for (var j = 0; j < badGuysCharactersArray.length; j++){
 }
 //enable click on good guy characters
 $(".goodGuyImageClass").on("click", function(){
-    if (goodGuyChosen === false) {
+    //check to see if good guy has already been chosen
+        if (goodGuyChosen === false) {
          goodGuyChosen = true;
     //store value of each good guy
-    var goodGuyFightingValue = ($(this).attr("dataGoodGuyImageValue"));
+    var goodGuyHealthPointsValue = ($(this).attr("dataGoodGuyImageValue"));
     //assign numeric value to good guy
-    goodGuyFightingValue = parseInt(goodGuyFightingValue);
+    goodGuyHealthPointsValue = parseInt(goodGuyHealthPointsValue);
     
     //assign name to good guy
     goodGuyName = ($(this)).attr("dataGoodGuyImageName");
@@ -59,28 +62,27 @@ $(".goodGuyImageClass").on("click", function(){
     else{
     alert("You have already chosen a Good Guy!");
     }
-    console.log(goodGuyName);
+    
 });
-console.log(goodGuyName);
+
 $(".badGuyImageClass").on("click", function(){
+    //check to see if bad guy has already been chosen
     if (badGuyChosen === false) {
         badGuyChosen = true;
     //store value of each bad guy
-    var badGuyFightingValue = ($(this).attr("dataBadGuyImageValue"));
+    var badGuyHealthPointsValue = ($(this).attr("dataBadGuyImageValue"));
     //assign numeric value to bad guy
-    badGuyFightingValue = parseInt(badGuyFightingValue);
-        
-     //assign name to bad guy
-    badGuyName = ($(this)).attr("dataBadGuyImageName");
+    badGuyHealthPointsValue = parseInt(badGuyHealthPointsValue);
     
+    //assign name to bad guy
+    badGuyName = ($(this)).attr("dataBadGuyImageName");
     $("#badGuyFighting").append(this);
     $("#badGuyNameDisplayed").html(badGuyName);
     }
     else{
         alert("You have already chosen a Bad Guy!");
         }
-        console.log(goodGuyName);
-        console.log(badGuyName);
+        
         //start duel function on button click
     $("#startDuel").on("click", function(){
      
@@ -92,36 +94,38 @@ $(".badGuyImageClass").on("click", function(){
           
         if (randomNumber === 0) {
           //action in duelingText window for good guy winning
-             $("#duelingText").append("<br><hr>" + goodGuyName + " shoots!");
-            
-            $("#duelingText").append("<br><hr>" + badGuyName + " shoots!");
-        
-             $("#duelingText").append("<br><hr>" + goodGuyName + " shoots!");
-        
-             $("#duelingText").append("<br><hr>" + badGuyName + " shoots!");
-         
-             $("#duelingText").append("<br><hr>" + goodGuyName + " shoots!");
-      
-             $("#duelingText").append("<br><hr>" + badGuyName + " is dead!");
-        setTimeout(function(){ alert(goodGuyName + " is the best Gunfighter in the West!!!"); }, 3000);
+        $("#duelingText").append("<br><hr>" + goodGuyName + " shoots!");
+        $("#duelingText").append("<br><hr>" + badGuyName + " shoots!");
+        $("#duelingText").append("<br><hr>" + goodGuyName + " shoots!");
+        $("#duelingText").append("<br><hr>" + badGuyName + " shoots!");
+        $("#duelingText").append("<br><hr>" + goodGuyName + " shoots!");
+        $("#duelingText").append("<br><hr>" + badGuyName + " is dead!");
+        setTimeout(function(){ alert(goodGuyName + " is the best Gunfighter in the West!!!"); }, 1500);
     }
         else {
               //action in duelingText window for bad guy winning
         $("#duelingText").append("<br><hr>" + badGuyName + " shoots first!");
-      
         $("#duelingText").append("<br><hr>" + goodGuyName + " shoots!");
-        
         $("#duelingText").append("<br><hr>" + badGuyName + " shoots!");
         $("#duelingText").append("<br><hr>" + goodGuyName + " shoots!");
-        
         $("#duelingText").append("<br><hr>" + badGuyName + " shoots!");
-        
         $("#duelingText").append("<br><hr>" + goodGuyName + " is dead!");
-        setTimeout(function(){ alert(badGuyName + " is the best Gunfighter in the West!!!"); }, 2000);
+        setTimeout(function(){ alert(badGuyName + " is the best Gunfighter in the West!!!"); }, 1500);
         }
         
         });
 });
-}
+function reset() {
+     //Variables reset
+     var goodGuyChosen = false;
+     var badGuyChosen = false;
+     var goodGuyName = "";
+     var badGuyName = ""
+     var randomNumber = 0;
+     var goodGuyNameDueling ="";
+     var delayButtonAlert;
+     }
+    resetButton.on('click', reset);
+ }
 startGame();
 });
